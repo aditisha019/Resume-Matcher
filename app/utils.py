6687d2649f57583ec48417fa3f7815b1de9ccf2c
Aditi Hotti <155968055+aditisha019@users.noreply.github.com>
2025-06-24 01:53:09 +0530

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,8 @@
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def compute_similarity(resume: str, jd: str) -> float:
+    embeddings = model.encode([resume, jd])
+    similarity = util.cos_sim(embeddings[0], embeddings[1])
+    return round(float(similarity[0][0]) * 100, 2)
